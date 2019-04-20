e87f395c6ba685854b79e1ca2e38337f2a75038f
user-input <mat.matheus_pa@hotmail.com>
2019-04-19 21:37:18 -0300

    Atualização um pequeno avanço no algoritmo mais grande passo para um funcionamento. - Remoção do nó, add busca criando nós e outros métodos derivados. - Correção de bugs - Criado novos bugs

--- src/projetomissionariosecanibais/Estado.java
+++ src/projetomissionariosecanibais/Estado.java
@@ -6,72 +6,86 @@ public class Estado {
     private int missionario; // 0 a n missionários
     private int canibal; // 0 a n canibais
     private boolean bote; //"false" - Direita / "true" - Esquerda
+    private int custo = 0;
+    Estado pai = null;//Armazena o estado de origem - Obs.: Caso tiver.
     
     //Remove 1 missionário ou canibal
     public boolean remove1Tipo(boolean tipo){
-        Estado temporario = this;
+               Estado temporario = this;
         if(tipo){ //true = missionário
             temporario.setMissionario(temporario.getMissionario()-1);
-            if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1){
-                setMissionario(missionario-1);
+            if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1 && missionario > 0){
+                missionario-=1;
+                custo++;
                 return true;
             }else{
                 return false;
             }
         }else{ //false =canibal
-            temporario.setMissionario(temporario.getMissionario()-1);
-            if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1){
-                setCanibal(canibal-1);
+            temporario.setCanibal(temporario.getCanibal()-2);
+            if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1 && canibal > 0){
+                canibal-=1;
+                custo++;
                 return true;
             }else{
                 return false;
             }
         }
     }
-    
+
     //Remove 2 missionários ou canibais
     public boolean remove2Tipo(boolean tipo){
         Estado temporario = this;
         if(tipo){ //true = missionário
             temporario.setMissionario(temporario.getMissionario()-2);
             if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1){
-                setMissionario(missionario-2);
+                missionario -=2;
+                custo++;
                 return true;
             }else{
                 return false;
             }
         }else{ //false =canibal
-            temporario.setMissionario(temporario.getMissionario()-2);
-            if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1){
-                setCanibal(canibal-2);
+            temporario.setCanibal(temporario.getCanibal()-2);
+            if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1 && canibal >= 2){
+                canibal -=2;
+                custo++;
                 return true;
             }else{
                 return false;
             }
         }
     }
-    
+
     //Remove 1 de cada
     public boolean remove1Cada(){
         Estado temporario = this;
         temporario.setMissionario(temporario.getMissionario()-1);
         temporario.setCanibal(temporario.getCanibal()-1);
         if(temporario.getCanibal() <= temporario.getMissionario() && canibal <= missionario-1){
-            setMissionario(missionario-1);
-            setCanibal(canibal-1);
+            missionario -=1;
+            canibal -=1;
+            custo++;
             return true;
         }else{
             return false;
         }
     }
-    
+
     //Construtor
-    public Estado(int missionario, int canibal, boolean bote) {
+    public Estado(int missionario, int canibal) { //Construtor inicio
+        this.missionario = missionario;
+        this.canibal = canibal;
+        this.bote = false;
+    }
+    
+    public Estado(int missionario, int canibal, boolean bote){ //Construtor para nó
         this.missionario = missionario;
         this.canibal = canibal;
         this.bote = bote;
-    }//
-    
+    }
+    //
+
     //Métodos gets e sets
     public int getMissionario() {
         return missionario;
@@ -93,6 +107,20 @@ public class Estado {
     public void setBote(boolean bote) {
         this.bote = bote;
     }
+
+    public int getCusto() {
+        return custo;
+    }
+    public void setCusto(int custo) {
+        this.custo = custo;
+    }
+
+    public Estado getPai() {
+        return pai;
+    }
+    public void setPai(Estado pai) {
+        this.pai = pai;
+    }    
     //
 
 
@@ -101,6 +129,4 @@ public class Estado {
         return "[M: " + missionario + "| C: " + canibal + "| B: " + bote + "]\n";
     }
 
-    
-    
 }
--- src/projetomissionariosecanibais/ProbMissionariosECanibais.java
+++ src/projetomissionariosecanibais/ProbMissionariosECanibais.java
@@ -2,9 +2,32 @@
 
 package projetomissionariosecanibais;
 
+import java.util.Scanner;
+
 public class ProbMissionariosECanibais {
 
     public static void main(String[] args) {
+        Estado atual = new Estado(3, 3, false);
+        Estado objetivo = new Estado(0, 0, true);
 
+        int quantMC = entradaAlg();
+        if(quantMC != -1){
+            Busca profundidade = new Busca(new Estado(quantMC, quantMC));
+        }else{
+            System.out.println("ERRO: Quantidade de criaturas inválida.");
+        }
+        
+    }
+    
+    public static int entradaAlg(){
+        Scanner tec = new Scanner(System.in);
+        
+        System.out.print("Digite a quantidade criaturas (Obs.: Somente número pares): ");
+        int quantCriaturas = tec.nextInt();
+        if(quantCriaturas%2 == 0){
+           return quantCriaturas /= 2;
+        }else{
+            return -1;
+        }
     }
 }
